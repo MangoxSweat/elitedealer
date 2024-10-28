c7d17e075349667cddb76f4a6fc87bcfbe86c164
Xavier Cooper <xcc2b@virginia.edu>
2024-10-28 13:59:19 +0000

    fix logo in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,12 @@
 <script>
 	import Nav from '$lib/components/Nav.svelte';
-	let src = '/logo.png';
+	let src = '/logo.webp';
 </script>
 
 <header>
 	<div id="logo">
 		<a href="/">
 			<picture>
-				<source srcset="/logo-small.png" media="(max-width: 600px)" />
-				<source srcset="/logo-large.png" media="(min-width: 601px)" />
 				<img {src} alt="Elite Dealer US Logo" />
 			</picture>
 		</a>
